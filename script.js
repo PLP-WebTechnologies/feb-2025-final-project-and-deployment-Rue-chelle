5b559473a1f1381eefafd854aba9a27b78a807a1
Alora <misshie21@gmail.com>
2025-05-09 15:24:56 +0200

    form validation

--- script.js
+++ script.js
@@ -107,3 +107,19 @@ document.addEventListener("DOMContentLoaded", () => {
     wrapper.appendChild(clone);
   });
 });
+
+document.getElementById("contact-form").addEventListener("submit", function (event) {
+  // Get form data
+  const name = event.target.name.value;
+  const email = event.target.email.value;
+  const message = event.target.message.value;
+
+  //  Log the data to the console
+  console.log("Name:", name);
+  console.log("Email:", email);
+  console.log("Message:", message);
+
+  //  Display a success message 
+  alert("Thank you for your message! We will get back to you soon😊.");
+
+});
